724e03536e99d6b916b050fb16af62f601f1afce
Clintina <clintinaroberts22@gmail.com>
2025-07-23 23:41:00 +0200

    Fixed query_samples.py file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,5 +13,5 @@ books_in_library = library.books.all()
 print("Books in", library_name + ":", list(books_in_library))
 
 # Retrieve the librarian for a library
-librarian = library.librarian
+librarian = Librarian.objects.get(library=library)
 print("Librarian for", library_name + ":", librarian)
