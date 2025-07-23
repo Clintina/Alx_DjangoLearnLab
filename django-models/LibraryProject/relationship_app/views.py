e3614371e97e832a7f55d717bc33e966e88669fb
Clintina <clintinaroberts22@gmail.com>
2025-07-24 00:03:22 +0200

    fixed views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from .models import Library
 from django.shortcuts import render
 from .models import Book, Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
 def list_books(request):
