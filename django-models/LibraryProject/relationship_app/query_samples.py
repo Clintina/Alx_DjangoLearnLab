5d1ef04faf7d5ee00a484b2c00d74f6d7b8955f0
Clintina <clintinaroberts22@gmail.com>
2025-07-23 23:39:04 +0200

    Add advaned model relationships and queries in django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,17 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
-print("Books by George Orwell:", list(books_by_author))
+print("Books by", author_name + ":", list(books_by_author))
 
 # List all books in a library
 library_name = "Central Library"
 library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
-print("Books in Central Library:", list(books_in_library))
+print("Books in", library_name + ":", list(books_in_library))
 
 # Retrieve the librarian for a library
 librarian = library.librarian
-print("Librarian for Central Library:", librarian)
+print("Librarian for", library_name + ":", librarian)
