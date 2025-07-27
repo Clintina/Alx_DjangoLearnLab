c19340ab0fe85baacf1aa64d39353f468f2ba3d5
Clintina <clintinaroberts22@gmail.com>
2025-07-27 03:01:05 +0200

    Added Relationship app files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+
 from .models import Book, Library
 
 # 📚 Book listing view
