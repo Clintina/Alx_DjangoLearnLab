def2e431d2c2fcd452bb90fa7f8a3d131d2f82ae
Clintina <clintinaroberts22@gmail.com>
2025-07-27 02:40:36 +0200

    Added Relationship app files

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -1,8 +1,7 @@
 from django.apps import AppConfig
-from .models import UserProfile
-
-
 
 class RelationshipAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
+
+    def ready(self):
+        import relationship_app.signals
