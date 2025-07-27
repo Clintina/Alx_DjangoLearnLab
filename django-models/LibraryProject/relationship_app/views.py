ac8b1484660896cf2f5547c592b5303ca35f5917
Clintina <clintinaroberts22@gmail.com>
2025-07-27 03:08:20 +0200

    Added Relationship app files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required  # 👈 must be on its own line
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
@@ -72,15 +72,15 @@ def member_view(request):
 
 # 🔒 Permission-protected book actions
 @permission_required('relationship_app.can_add_book')
-def add_book_view(request):
+def add_book(request):
     return render(request, 'relationship_app/add_book.html')
 
 @permission_required('relationship_app.can_change_book')
-def edit_book_view(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
 @permission_required('relationship_app.can_delete_book')
-def delete_book_view(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
     return render(request, 'relationship_app/delete_book.html', {'book': book})
