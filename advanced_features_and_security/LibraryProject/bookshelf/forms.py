007b63859549c034a7b486a78357b33951aa4171
Clintina <clintinaroberts22@gmail.com>
2025-08-01 23:30:21 +0200

    corrected forms.py folder

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,20 @@
-from django import forms
+# bookshelf/forms.py
 
-class BookSearchForm(forms.Form):
-    title = forms.CharField(max_length=100)
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Enter title'}),
+            'author': forms.TextInput(attrs={'placeholder': 'Enter author'}),
+            'publication_year': forms.NumberInput(attrs={'placeholder': 'Enter year'}),
+        }
+
+    def clean_publication_year(self):
+        year = self.cleaned_data.get('publication_year')
+        if year and year < 0:
+            raise forms.ValidationError("Year must be a positive number.")
+        return year
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,35 +1,58 @@
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render  get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookSearchForm
+from .forms import BookSearchForm, BookForm
 
-
+# Permission-protected view: list books
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+
+# Permission-protected view: create a book
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
-    # Dummy logic for now
-    return render(request, 'bookshelf/create_book.html')
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
+
+# Permission-protected view: edit a book
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    # Dummy logic for now
-    return render(request, 'bookshelf/edit_book.html', {'book': book})
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
+
+# Permission-protected view: delete a book
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    # Dummy logic for now
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book-list')
     return render(request, 'bookshelf/delete_book.html', {'book': book})
 
+
+# Safe search view using form + ORM
+@permission_required('bookshelf.can_view', raise_exception=True)
 def search_view(request):
     form = BookSearchForm(request.GET or None)
     books = []
     if form.is_valid():
         title = form.cleaned_data.get('title')
-        books = Book.objects.filter(title__icontains=title)  # Safe query
+        books = Book.objects.filter(title__icontains=title)
     return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
