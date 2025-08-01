57142d035b64aa936fa8202fa098fa72314dbf8d
Clintina <clintinaroberts22@gmail.com>
2025-08-01 23:35:36 +0200

    Fixed forms.py file

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,9 @@
-# bookshelf/forms.py
-
 from django import forms
 from .models import Book
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
 
 class BookForm(forms.ModelForm):
     class Meta:
