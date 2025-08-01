bb13a1e0c1b613fcf8385abc5c699ed6a057f2c9
Clintina <clintinaroberts22@gmail.com>
2025-08-01 23:47:13 +0200

    Fixed views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,12 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookSearchForm, BookForm, ExampleForm
+from .forms import BookSearchForm, BookForm, ExampleForm 
 
-# Permission-protected view: list books
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
-
-
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
@@ -22,35 +19,9 @@ def create_book(request):
         form = BookForm()
     return render(request, 'bookshelf/form_example.html', {'form': form})
 
-
-
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
-            form.save()
-            return redirect('book-list')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'bookshelf/form_example.html', {'form': form})
-
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('book-list')
-    return render(request, 'bookshelf/delete_book.html', {'book': book})
-
-
-@permission_required('bookshelf.can_view', raise_exception=True)
-def search_view(request):
-    form = BookSearchForm(request.GET or None)
-    books = []
-    if form.is_valid():
-        title = form.cleaned_data.get('title')
-        books = Book.objects.filter(title__icontains=title)
-    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
