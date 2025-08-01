8e81d178fb6ded137bfe4529e3b1161143d3e9ab
Clintina <clintinaroberts22@gmail.com>
2025-08-01 23:54:12 +0200

    Fixed views.py file

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookSearchForm, BookForm, ExampleForm 
+from .forms import ExampleForm
+from .forms import BookSearchForm, BookForm 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
