aab526df2c210644cf31fde8924b07002bbda474
Clintina <clintinaroberts22@gmail.com>
2025-08-01 20:53:14 +0200

    Restructured models.py file

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'relationship_app', 
     'accounts', 
 ]
-AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
