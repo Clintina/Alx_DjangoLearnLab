e98c4e202034956df8702c4d3df361814f382cab
Clintina <clintinaroberts22@gmail.com>
2025-08-09 10:22:29 +0200

    Add Author and Book models with custom serializers

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
+from django.utils import timezone
 from .models import Author, Book
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'publication_year']
+        fields = '__all__'
+
+    
+    def validate_publication_year(self, value):
+        current_year = timezone.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                f"Publication year {value} cannot be in the future."
+            )
+        return value
+
 
 class AuthorSerializer(serializers.ModelSerializer):
+    
     books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
-        fields = ['id', 'name', 'books']
+        fields = ['name', 'books']
