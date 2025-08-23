742edddf37131a186d9632374d58046a933e7340
Clintina <clintinaroberts22@gmail.com>
2025-08-23 21:59:36 +0200

    Add authentication documentation with simplified flow for register, login, logout, and profile

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include 
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('', include('django.contrib.auth.urls')),
 ]
