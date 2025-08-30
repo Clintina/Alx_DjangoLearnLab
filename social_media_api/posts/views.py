a3d9dd9bf5c370a0efb4b70c25a7babb8d98c883
Clintina <clintinaroberts22@gmail.com>
2025-08-30 13:26:18 +0200

    Fixed posts/views.py file

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import generics
 from django.contrib.auth import get_user_model
@@ -8,7 +7,7 @@ from .models import Post, Comment, Like, Notification
 from .serializers import PostSerializer, CommentSerializer
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def like_post(request, pk):
     post = generics.get_object_or_404(Post, pk=pk)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
